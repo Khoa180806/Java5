94b60062e30a3cc682d21a659b8e96f89b3c9daf
Khoa180806 <khoaphan180806@gmail.com>
2026-01-17 18:04:33 +0700

    Lab4_Bai2

--- src/main/java/com/java4/study/Lab4/Staff.java
+++ src/main/java/com/java4/study/Lab4/Staff.java
@@ -1,5 +1,10 @@
 package com.java4.study.Lab4;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,19 +18,26 @@ import java.util.Date;
 @Builder
 @Data
 public class Staff {
+    @NotBlank(message = "Chưa nhập email")
+    @Email(message = "Email không đúng định dạng")
     private String id;
+    @NotBlank(message = "Chưa nhập họ và tên")
     private String fullname;
     @Builder.Default
     private String photo = "default-avatar.png";
     @Builder.Default
+    @NotNull(message = "Chưa chọn giới tính")
     private Boolean gender = false;
     @Builder.Default
+    @NotNull(message = "Chưa nhập ngày sinh")
+    @Past(message = "Ngày sinh không hợp lệ")
     @DateTimeFormat(
             pattern = "MM/dd/yyyy")
     private Date birthday = null; // Consider requiring this field to be set explicitly
     @Builder.Default
-    private double salary =
-            12345.6789;
+    @Min(value = 1000, message = "Lương tối thiểu phải là 1000")
+    @NotNull(message = "Chưa nhập lương")
+    private double salary = 1000;
     @Builder.Default
     private Integer level = 0;
 }
--- src/main/java/com/java4/study/Lab4/controller/StaffController2.java
+++ src/main/java/com/java4/study/Lab4/controller/StaffController2.java
@@ -1,8 +1,10 @@
 package com.java4.study.Lab4.controller;
 
 import com.java4.study.Lab4.Staff;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -17,13 +19,18 @@ public class StaffController2 {
     }
 
     @RequestMapping("/staff/create/save")
-    public String createSave(Model model, @ModelAttribute("staff") Staff staff,
-                             @RequestPart("photo_file") MultipartFile photoFile) {
-// Gán tên file upload cho thuộc tính photo của bean nếu có upload file
+    public String createSave(Model model,
+                             @RequestPart("photo_file") MultipartFile photoFile,
+                             @Valid @ModelAttribute("staff") Staff staff, Errors errors) {
         if (!photoFile.isEmpty()) {
             staff.setPhoto(photoFile.getName());
         }
-        model.addAttribute("message", "Xin chào " + staff.getFullname());
-        return "/Lab4/staff-create";
+        if (errors.hasErrors()) {
+            model.addAttribute("message", "Vui lòng sửa các lỗi sau!");
+
+        } else {
+            model.addAttribute("message", "Dữ liệu đã nhập đúng!");
+        }
+        return "/Lab4/staff-validate";
     }
 }
